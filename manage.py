d3e2f5c114bedf132c3350d0f41266136d7a6136
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-04 16:34:09 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,15 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
+from pathlib import Path
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moodmusics.settings')
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moodmusics.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Django is not installed or not found in your environment.\n"
+            "👉 Activate your virtual environment and install dependencies:\n"
+            "   pip install -r requirements.txt"
         ) from exc
+
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
